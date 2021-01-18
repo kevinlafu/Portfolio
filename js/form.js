2356f2fed5bafa8ba498617be7209c883ab671e5
kevin.reinhardt <kevinlafu.96@gmail.com>
2021-01-18 13:41:06 -0300

    form fetch fix

--- js/form.js
+++ js/form.js
@@ -1,51 +1,91 @@
-$(function () {
-  // Get the form.
-  var form = $("#ajax-contact");
+document.querySelector("form").addEventListener("submit", handleSubmit);
 
-  // Get the messages div.
+const handleSubmit = (e) => {
+  e.preventDefault();
+  let myForm = document.getElementById("ajax-contact");
   var formMessages = $("#form-messages");
 
-  // Set up an event listener for the contact form.
-  $(form).submit(function (e) {
-    // Stop the browser from submitting the form.
-    e.preventDefault();
+  let formData = new FormData(myForm);
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => {
+      // Make sure that the formMessages div has the 'success' class.
+      $(formMessages).removeClass("bg-danger");
+      $(formMessages).addClass("bg-success");
 
-    // Serialize the form data.
-    var formData = $(form).serialize();
+      // Set the message text.
+      $(formMessages).text("Your message successfully sent");
 
-    // Submit the form using AJAX.
-    $.ajax({
-      type: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      url: "/",
-      data: formData,
+      // Clear the form.
+      $("#name, #email, #message").val("");
     })
-      .done(function (response) {
-        // Make sure that the formMessages div has the 'success' class.
-        $(formMessages).removeClass("bg-danger");
-        $(formMessages).addClass("bg-success");
+    .catch((error) => {
+      // Make sure that the formMessages div has the 'error' class.
+      $(formMessages).removeClass("bg-success");
+      $(formMessages).addClass("bg-danger");
 
-        // Set the message text.
-        $(formMessages).text("Your message successfully sent");
+      // Set the message text.
+      if (data.responseText !== "") {
+        $(formMessages).text(data.responseText);
+      } else {
+        $(formMessages).text(
+          "Oops! An error occured and your message could not be sent."
+        );
+      }
+    });
+};
 
-        // Clear the form.
-        $("#name, #email, #message").val("");
-      })
-      .fail(function (data) {
-        // Make sure that the formMessages div has the 'error' class.
-        $(formMessages).removeClass("bg-success");
-        $(formMessages).addClass("bg-danger");
+// $(function () {
+//   // Get the form.
+//   var form = $("#ajax-contact");
 
-        // Set the message text.
-        if (data.responseText !== "") {
-          $(formMessages).text(data.responseText);
-        } else {
-          $(formMessages).text(
-            "Oops! An error occured and your message could not be sent."
-          );
-        }
-      });
-  });
-});
+//   // Get the messages div.
+//   var formMessages = $("#form-messages");
+
+//   // Set up an event listener for the contact form.
+//   $(form).submit(function (e) {
+//     // Stop the browser from submitting the form.
+//     e.preventDefault();
+
+//     // Serialize the form data.
+//     var formData = $(form).serialize();
+
+//     // Submit the form using AJAX.
+//     $.ajax({
+//       type: "POST",
+//       headers: {
+//         "Content-Type": "application/x-www-form-urlencoded",
+//       },
+//       url: "/",
+//       data: formData,
+//     })
+//       .done(function (response) {
+//         // Make sure that the formMessages div has the 'success' class.
+//         $(formMessages).removeClass("bg-danger");
+//         $(formMessages).addClass("bg-success");
+
+//         // Set the message text.
+//         $(formMessages).text("Your message successfully sent");
+
+//         // Clear the form.
+//         $("#name, #email, #message").val("");
+//       })
+//       .fail(function (data) {
+//         // Make sure that the formMessages div has the 'error' class.
+//         $(formMessages).removeClass("bg-success");
+//         $(formMessages).addClass("bg-danger");
+
+//         // Set the message text.
+//         if (data.responseText !== "") {
+//           $(formMessages).text(data.responseText);
+//         } else {
+//           $(formMessages).text(
+//             "Oops! An error occured and your message could not be sent."
+//           );
+//         }
+//       });
+//   });
+// });
