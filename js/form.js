2c13bfded2b8c9bf98c33ff3eac6f3a0e030bc64
kevin.reinhardt <kevinlafu.96@gmail.com>
2021-01-18 12:28:44 -0300

    cors fix 2

--- js/form.js
+++ js/form.js
@@ -17,7 +17,7 @@ $(function () {
     $.ajax({
       type: "POST",
       headers: {
-        "Access-Control-Allow-Origin": "https://getsimpleform.com/",
+        "Access-Control-Allow-Origin": "https://getsimpleform.com",
       },
       url: $(form).attr("action"),
       data: formData,
